5967ffb3af09c244b42de5860651a99298d4debc
lakses <ivlasov981@gmail.com>
2025-02-19 20:20:07 +0900

    create

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .Form import RegistrationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    pass
