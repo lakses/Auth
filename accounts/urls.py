98865a2b07d413ad0d696aa8dda18861eee70bf4
lakses <ivlasov981@gmail.com>
2025-02-21 17:51:32 +0900

    login add

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import register
+from .views import register,home,login_view
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', login_view, name='login'),
+    path('home/',home, name = 'home')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import login, authenticate
+from django.contrib import messages
 from .Form import RegistrationForm
 
 def register(request):
@@ -8,10 +9,26 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            messages.success(request, 'Вы успешно зарегистрированы и вошли в систему.')
+            return redirect('home') 
     else:
         form = RegistrationForm()
     return render(request, 'register.html', {'form': form})
 
 def login_view(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user) 
+            messages.success(request, 'Вы успешно вошли в систему.')
+            return redirect('home')  
+        else:
+            messages.error(request, 'Неверный логин или пароль.')
+
+    return render(request, 'login.html')  
+
+def home(request):
+    return HttpResponse('<h1>Круто</h1>') 
