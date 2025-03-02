603eb366d6711126e6e514cf42ace810c26893be
lakses <ivlasov981@gmail.com>
2025-03-02 09:18:00 +0900

    dashe ne budet, v novgorod ne letim

--- forum/views.py
+++ forum/views.py
@@ -23,14 +23,23 @@ def post_create(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = post.comments.all()
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            return redirect('post_detail', post_id=post.id)
-    else:
-        form = CommentForm()
-    return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'form': form})
+    form = None
+
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                return redirect('post_detail', post_id=post.id)
+        else:
+            form = CommentForm()
+
+    return render(request, 'post_detail.html', {
+        'post': post,
+        'comments': comments,
+        'form': form,
+        'user_is_authenticated': request.user.is_authenticated
+    })
